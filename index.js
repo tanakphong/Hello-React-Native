c8ef0ead011ea83fe1b2385f7fdbf58d69c6c0e0
tanakphong <t.phongsing@gmail.com>
2018-08-25 01:16:53 +0700

    Learn Height and Width

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './Style';
+import App from './FlexDirectionBasics';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
